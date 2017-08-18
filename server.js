41d856c2fa4aac536a1f3206b556c96bb48800ed
David Lewis <david.lewis@wayveapp.com>
2017-08-18 12:50:32 +0100

    Allow origin header for server

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   const principal = req.query.principal;
   const periodicDeposit = req.query.periodicDeposit;
